ae3b49ddfd82824bffa4c50b96db9ed300e1ca76
Alastair Hughes <hobbitalastair@gmail.com>
2014-11-24 10:45:23 +1300

    Added computer db

---
+++ computers/toshiba_310cds.sh
@@ -0,0 +1,14 @@
+################################################################################
+# Information file for a Toshiba 310CDS computer
+#
+# Author: Alastair Hughes
+# Contact: < hobbitalastair at yandex dot com >
+# Date: 24-11-2014
+################################################################################
+
+export ${_computer}_name='toshiba_310cds'
+export ${_computer}_cpu='pentium'
+export ${_computer}_arch='i586'
+export ${_computer}_flags='-mmmx -Os'
+export ${_computer}_cores='1'
+

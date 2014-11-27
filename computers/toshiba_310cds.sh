8fa6af2c15ada109622322e0b139da6b04299f0e
Alastair Hughes <hobbitalastair@gmail.com>
2014-11-27 19:11:37 +1300

    Fixed a couple of issues in the computer description scripts

--- computers/toshiba_310cds.sh
+++ computers/toshiba_310cds.sh
@@ -7,7 +7,7 @@
 ################################################################################
 
 export ${_computer}_name='toshiba_310cds'
-export ${_computer}_cpu='pentium'
+export ${_computer}_cpu='pentium-mmx' # Apparently the -mmx is correct!
 export ${_computer}_arch='i586'
 export ${_computer}_flags='-mmmx -Os'
 export ${_computer}_cores='1'

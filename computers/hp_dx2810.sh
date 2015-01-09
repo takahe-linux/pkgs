d5b87458006cbd410b8e76fd338ffc459c83f8aa
Alastair Hughes <hobbitalastair@gmail.com>
2015-01-09 13:43:00 +1300

    Changed back. I really need a better way...

--- computers/hp_dx2810.sh
+++ computers/hp_dx2810.sh
@@ -8,8 +8,8 @@
 
 export ${_computer}_name="hp_dx2810"
 export ${_computer}_cpu='core2'
-export ${_computer}_arch='i586'
-#export ${_computer}_arch='x86_64'
+#export ${_computer}_arch='i586'
+export ${_computer}_arch='x86_64'
 export ${_computer}_flags='-pipe -O2' # Because it has lots of memory
 export ${_computer}_cores='2'
 

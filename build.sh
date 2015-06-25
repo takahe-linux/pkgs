8698710fc8be090ddcb5c2e0fa698922b1f86ccc
Alastair Hughes <hobbitalastair@gmail.com>
2015-06-25 19:08:07 +1200

    Refactored and hopefully fixed all of the bugs...

--- build.sh
+++ build.sh
@@ -59,6 +59,63 @@ check_arg_numbers() {
 
 
 #
+# Define the helper build functions
+#
+
+build_package() {
+    # Build a given package with the given arguments for makepkg and pacman.
+    # Usage: build_package <name> <prefix>
+    # Influential variables: $pacman_args, $makepkg_args
+
+    check_arg_numbers 2 $@
+
+    name="$1"
+    prefix="$2"
+
+    case "${prefix}" in
+        toolchain) target="local";;
+        packages) target="target";;
+        *) echo "Unknown target '${prefix}'!"; exit 1;;
+    esac
+
+    message "Building ${target} package ${prefix}/${name}..."
+
+    cd "${rootdir}/${prefix}/${name}"
+
+    # Figure out the names of the resulting packages.
+    names="$("${rootdir}/scripts/pkgnames" "PKGBUILD")"
+
+    # Rebuild if need be.
+    if need_rebuild ${names}; then
+        message "Rebuilding ${prefix}/${name}!"
+
+        # Build the package
+        "${noop}" || yes 'y' | makepkg --config ../makepkg.conf -cC \
+            ${makepkg_args}
+    fi
+
+    # Install all of the resulting packages.
+    # Also, keep track of the oldest package; this is used elsewhere in the
+    # script, and will be until I get proper dependencies. 
+    previous_package_date="0" # The timestamp on the packages...
+    for pkgname in ${names}; do
+        if ! "${noop}"; then
+            if [ ! -e "${pkgname}" ]; then
+                message "Expected to find a package in '$(pwd)' called '${pkgname}'!"
+                exit 1
+            fi
+            yes 'y' | sudo pacman ${pacman_args} -U "${pkgname}" > /dev/null
+        fi
+
+        # Check the timestamp on the package...
+        local package_date="$(stat -c '%Y' "${pkgname}")"
+        if [ "${package_date}" -gt "${previous_package_date}" ]; then
+            previous_package_date="${package_date}"
+        fi
+    done
+}
+
+#
 # Define the build functions
 #
 
@@ -84,32 +141,9 @@ build_local() {
 
     check_arg_numbers 2 $@
 
-    message "Building toolchain package ${prefix}/${name}..."
-
-    cd "${rootdir}/${prefix}/${name}"
-
-    # Figure out the names of the resulting packages.
-    names="$("${rootdir}/scripts/pkgnames" "PKGBUILD")"
-
-    # Rebuild if need be.
-    if need_rebuild ${names}; then
-        message "Rebuilding ${prefix}/${name}!"
-
-        # Build the package
-        "${noop}" || yes 'y' | makepkg --config ../makepkg.conf -srcfC
-    fi
-
-    # Exit as needed.
-    "${noop}" && return 0
-
-    # Install all of the resulting packages.
-    for pkgname in ${names}; do
-        if [ ! -e "${pkgname}" ]; then
-            message "Expected to find a package in '$(pwd)' called '${pkgname}'!"
-            exit 1
-        fi
-        yes 'y' | sudo pacman -U "${pkgname}" > /dev/null
-    done
+    makepkg_args="-srf" \
+        pacman_args="" \
+        build_package "${1}" "${2}"
 }
 
 build_target() {
@@ -117,34 +151,10 @@ build_target() {
 
     check_arg_numbers 2 $@
 
-    message "Building target package ${prefix}/${name}"
-
-
-    cd "${rootdir}/${prefix}/${name}"
-
-    # Figure out the names of the resulting packages.
-    names="$("${rootdir}/scripts/pkgnames" "PKGBUILD")"
-
-    # Rebuild if need be.
-    if need_rebuild "${names}"; then
-        message "Rebuilding ${prefix}/${name}!"
-
-        # Build the package
-        "${noop}" || makepkg --config ../makepkg.conf -dfCcL --nocheck
-    fi
-
-    # Exit as needed.
-    "${noop}" && return 0
-
-    # Install all of the resulting packages.
-    for pkgname in ${names}; do
-        if [ ! -e "${pkgname}" ]; then
-            message "Expected to find a package in '$(pwd)' called '${pkgname}'!"
-            exit 1
-        fi
-        sudo pacman --config "${rootdir}/pacman.conf" -U "${pkgname}" --noconfirm \
-            > /dev/null
-    done
+    # Build the package...
+    makepkg_args="-dLf --nocheck" \
+        pacman_args="--config ${rootdir}/pacman.conf --noconfirm" \
+        build_package "${1}" "${2}"
 }
 
 need_rebuild() {
@@ -153,29 +163,40 @@ need_rebuild() {
     # It will return 0 (true) if the packages need rebuilding, 1 (false) if
     # they don't.
 
-    # Check if the build is already 'dirty'.
-    if [ "$(cat "${STATUS_FILE}" | grep 'DIRTY' | sed 's:DIRTY=::')" == 'true' ]; then
-        return 0
-    fi
-
-    # Check for a cached version.
-    local pkgage="$(stat -c "%Y" PKGBUILD)"
-    for pkgname in ${1}; do
-        # Check that a symlink exists, and that the symlink is newer than the
-        # pkgbuild.
-        if [ ! -e "${pkgname}" ] || [ "$(stat -c "%Y" "${pkgname}")" -lt "${pkgage}" ]; then
-            make_dirty
+    # Check that there exists an up-to-date package.
+    local pkgdate="$(date '+%s')" # Set to now; time of youngest package.
+    for pkgname in ${@}; do
+        # Check that a symlink exists.
+        if [ ! -e "${pkgname}" ]; then
             return 0
+        fi
+
+        # Update the youngest age of the packages...
+        local currentpkgdate="$(stat -c "%Y" "${pkgname}")"
+        if [ "${pkgdate}" -gt "${currentpkgdate}" ]; then
+            pkgdate="${currentpkgdate}"
         fi
     done
 
+    # Check that the package is up to date.
+    local pkgbuilddate="$(stat -c "%Y" PKGBUILD)" # The age of the PKGBUILD.
+    if [ "${pkgbuilddate}" -gt "${pkgdate}" ]; then
+        return 0
+    fi
+
+    # Check if the prior package is _newer_ than this one...
+    # This fixes the 'hacky' use of a DIRTY flag, and also fixes the bug where
+    # the build system would not rebuild packages in the case of the build
+    # being interrupted and restarted!
+    #TODO: This is basically dependency related; if any of the deps are newer,
+    #      rebuild. Instead, I'm hacking this... because deps are currently
+    #      borked.
+    if [ -n "${previous_package_date}" ] && [ "${previous_package_date}" -gt "${pkgdate}" ]; then
+        return 0
+    fi
+    
+
     return 1
-}
-
-make_dirty() {
-    # Identify the build as being 'dirty' from this point onwards.
-
-    echo "DIRTY=true" >> "${STATUS_FILE}"
 }
 
 #TODO: Add argument parsing for more functionality
@@ -183,9 +204,6 @@ make_dirty() {
 # --rebuild-all
 # --rebuild=<packages> # Just 'touch' the PKGBUILD's?
 # --???
-
-# Create a shared status file.
-STATUS_FILE="$(mktemp --suffix="-$(basename "${0}")")"
 
 # Setup sudo_keepalive
 #TODO: Remove this... building using root _is_ dangerous!
@@ -200,8 +218,5 @@ done
 # Clean up the helper processes
 kill "${KEEPALIVE_PID}"
 wait
-
-# Remove the status file.
-rm "${STATUS_FILE}"
 
 message "Cleaned up and exited!"

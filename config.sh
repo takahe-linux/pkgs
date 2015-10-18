830279ec382cc097808634c331e93d22cfc3b192
Alastair Hughes <hobbitalastair@gmail.com>
2015-10-18 16:38:34 +1300

    Removed -fno-expections (why was it there at all?)

--- config.sh
+++ config.sh
@@ -66,7 +66,7 @@ _sysroot="${_toolroot}/sysroot"
 #
 
 # Target CFLAGS and ldflags
-_target_cflags="-g -Os -ffunction-sections -fno-exceptions -fdata-sections"
+_target_cflags="-g -Os -ffunction-sections -fdata-sections"
 _target_ldflags="-Wl,-rpath,${_sysroot}${_libdir},--sysroot,${_sysroot},--gc-sections,-dynamic-linker,${_libdir}/ld-musl-${_target_arch}.so.1"
 
 

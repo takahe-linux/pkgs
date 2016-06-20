1624846dc533d2a6cc1699a4414207888207e59b
Alastair Hughes <hobbitalastair@gmail.com>
2016-06-20 16:28:30 +1200

    Use an initscript

---
+++ packages/initscripts/init.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/sh
+
+# Mount the expected filesystems.
+mount -t proc proc /proc
+mount -t sysfs sys /sys
+mount -a
+
+# Apply the hostname.
+hostname -F /etc/hostname
+
+# Initialise the devices.
+mdev -s
+ln -s /proc/self/fd /dev/fd
+
+# Run the helper script.
+while true; do
+    /etc/init/run
+done
---
+++ packages/initscripts/shutdown.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/sh
+# TODO: Kill/wait for remaining services.
+poweroff -f
